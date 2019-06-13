177b1e3105e93b0321c6b2087b60cf1e1784b5f9
Haim D <haim11293@aol.com>
2019-06-13 02:34:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
   # your code here
   counter = 0
   while counter < array.length do
-    puts array[counter]**2
+    array[counter]**2
     counter += 1
   end
 end
-square_array(aray)
